5a647cbbdb6a7a5a22cce999f1d651e29c491ff1
vitaly <vitalikstain@inbox.ru>
2024-08-14 19:52:11 +0300

    refactor: get rid of N+1 during getting credit list

--- apps/credit/serializers/credit.py
+++ apps/credit/serializers/credit.py
@@ -1,19 +1,11 @@
 from rest_framework import serializers
 
 from apps.credit.models import Credit
-from apps.manufacturer.models import Manufacturer
 
 
 class CreditSerializer(serializers.ModelSerializer):
-    unique_manufactures = serializers.SerializerMethodField(read_only=True)
+    unique_manufactures = serializers.ListField()
 
     class Meta:
         model = Credit
         fields = ["id", "unique_manufactures"]
-
-    def get_unique_manufactures(self, obj: Credit) -> list[int]:
-        return (
-            Manufacturer.objects.filter(products__credit=obj)
-            .distinct()
-            .values_list("id", flat=True)
-        )
