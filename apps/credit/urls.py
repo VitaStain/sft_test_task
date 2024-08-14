e4be74bc0661a4da00049e9d25204d6e0cb0533f
vitaly <vitalikstain@inbox.ru>
2024-08-14 18:13:55 +0300

    feat: CreditListView endpoint

--- apps/credit/urls.py
+++ apps/credit/urls.py
@@ -1,9 +1,14 @@
 from django.urls import include, path
 
+from apps.credit.views.credit import CreditListView
 
 urlpatterns = [
     path(
         "v1/",
-        include([]),
+        include(
+            [
+                path("credits/", CreditListView.as_view()),
+            ]
+        ),
     ),
 ]
