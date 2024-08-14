902312a8bc383555f2cc6fe0ee05471d508dbb07
vitaly <vitalikstain@inbox.ru>
2024-08-14 18:12:49 +0300

    feat: CreditListView endpoint

---
+++ apps/credit/serializers/credit.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from apps.credit.models import Credit
+from apps.manufacturer.models import Manufacturer
+
+
+class CreditSerializer(serializers.ModelSerializer):
+    unique_manufactures = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Credit
+        fields = "__all__"
+
+    def get_unique_manufactures(self, obj: Credit) -> list[int]:
+        return (
+            Manufacturer.objects.filter(products__credit=obj)
+            .distinct()
+            .values_list("id", flat=True)
+        )
---
+++ apps/credit/views/credit.py
@@ -0,0 +1,21 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import generics
+
+from apps.credit.models import Credit
+from apps.credit.serializers.credit import CreditSerializer
+
+
+@extend_schema(tags=["credits"])
+@extend_schema_view(
+    get=extend_schema(summary="Get credits"),
+)
+class CreditListView(generics.ListAPIView):
+    queryset = Credit.objects.all()
+    serializer_class = CreditSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = {
+        "contract__id": ["exact"],
+    }
